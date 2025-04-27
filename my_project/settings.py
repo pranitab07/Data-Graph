48c5aff10c26c7507fdcbff157a9648bb817774e
Pranita Bobade <141933843+pranitab07@users.noreply.github.com>
2025-04-27 18:45:24 +0530

    Update settings.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -22,6 +22,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'home.apps.homeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -32,6 +33,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
